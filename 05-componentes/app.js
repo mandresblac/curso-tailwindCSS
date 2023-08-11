4b825ac51b599747d999b23eb64b6cb6d107d32d
manuel blanco <mandresblac@gmail.com>
2023-08-11 12:22:43 -0500

    commit 11

--- 05-componentes/app.js
+++ 05-componentes/app.js
@@ -2,6 +2,6 @@ const boton = document.querySelector("#boton");
 const menu = document.querySelector("#menu");
 
 boton.addEventListener("click", () => {
-  // .toggle() quita o agrega la clase "hidden", es decir la intercambia, osea que muestra u oculta los enlces de la barra de navegación
+  // .toggle() quita o agrega la clase "hidden", es decir la intercambia, osea que muestra u oculta los enlaces de la barra de navegación
   menu.classList.toggle("hidden");
 });
