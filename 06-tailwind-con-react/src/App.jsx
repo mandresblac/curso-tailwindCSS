87a4570b0738423a85f536d00ad86283a2dd7103
manuel blanco <mandresblac@gmail.com>
2023-08-11 14:23:09 -0500

    commit 13

--- 06-tailwind-con-react/src/App.jsx
+++ 06-tailwind-con-react/src/App.jsx
@@ -1,13 +1,20 @@
-import './App.css'
+import './index.css';
+import Card from './components/Card';
 
 function App() {
 
   return (
     <>
-      <div className='container mx-auto mt-5'>
-        <h1>Hola mundo</h1>
-        <h2>Tailwind CSS</h2>
-        <button className='btn btn-blue btn-blue:hover'>Boton</button>
+      <div className='flex -mx-2 flex-wrap mt-10'>
+        <div className='w-full sm:w-1/2 md:w-1/3 px-2'>
+          <Card photo='https://picsum.photos/900' />
+        </div>
+        <div className='w-full sm:w-1/2 md:w-1/3 px-2'>
+          <Card photo='https://picsum.photos/900' />
+        </div>
+        <div className='w-full sm:w-1/2 md:w-1/3 px-2'>
+          <Card photo='https://picsum.photos/900' />
+        </div>
       </div>
     </>
   )
